314fc1e3ab0fca0cc7f78bea393f4f07d51db192
archieamas11 <archiealbarico69@gmail.com>
2025-11-24 10:43:40 +0800

    ✨ feat(contact): Enhance contact section with personal details
    - Updated layout and styling for better presentation
    - Integrated personal details for improved user engagement
    - Removed unused constants file

--- src/pages/about/constants.ts
+++ src/pages/about/constants.ts
@@ -57,7 +57,7 @@ export const personalDetails = [
   { icon: FaTelegramPlane, label: 'Telegram', value: 'rico', href: 'https://t.me/rico' },
   { icon: BsDiscord, label: 'Discord', value: 'archieamas11', href: 'https://discord.com/users/archieamas11' },
   { icon: Phone, label: 'Phone', value: '+63 963 463 6306', href: 'tel:+639634636306' },
-  { icon: Github, label: 'GitHub', value: 'archieamas11', href: 'https://github.com/archieamas11' },
+  { icon: Github, label: 'Github', value: 'archieamas11', href: 'https://github.com/archieamas11' },
   { icon: Mail, label: 'Email', value: 'archiealbarico69@gmail.com', href: 'mailto:archiealbarico69@gmail.com' },
 ]
 
--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,17 +1,60 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import ContactForm from './Forms'
+import { personalDetails } from '../about/constants'
 
 export function ContactSection() {
   return (
     <section id="contact" className="section-wrapper">
       <div className="grid w-full grid-cols-1 justify-between gap-20 lg:grid-cols-2 lg:gap-0">
         {/* Left content*/}
-        <div>
-          <h2 className="mb-4 text-3xl font-bold md:text-5xl">Get In Touch</h2>
-          <p className="text-muted-foreground font-base max-w-md text-lg md:text-xl">
+        <div className="p-0 lg:p-25">
+          <h2 className="mb-4 text-3xl font-bold sm:text-4xl md:text-5xl lg:text-6xl">Get In Touch</h2>
+          <p className="text-muted-foreground font-base max-w-md text-justify text-lg md:text-xl">
             I'm always interested in new opportunities. Whether you want to collaborate, discuss a project, or just say hello, I'd love to hear from
             you!
           </p>
+          <div className="mt-10 flex flex-col gap-2">
+            {personalDetails
+              .filter((detail) => ['Facebook', 'Phone', 'Email'].includes(detail.label))
+              .map((detail) => (
+                <a
+                  key={detail.label}
+                  href={detail.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="group flex gap-2 space-y-2 text-sm transition-colors"
+                >
+                  <div className="bg-primary/10 text-primary flex h-10 w-10 shrink-0 items-center justify-center rounded-full text-center transition-colors">
+                    <detail.icon size={15} />
+                  </div>
+                  <div>
+                    <p className="font-medium">{detail.label}</p>
+                    <p className="text-muted-foreground text-sm">{detail.value}</p>
+                  </div>
+                </a>
+              ))}
+          </div>
+
+          <div className="mt-5 space-y-4">
+            <div className="text-foreground text-xl font-bold tracking-tight">Follow Me</div>
+            <div className="flex gap-2">
+              {personalDetails
+                .filter((detail) => ['Telegram', 'Discord', 'Github', 'Instagram'].includes(detail.label))
+                .map((detail) => (
+                  <a
+                    key={detail.label}
+                    href={detail.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="group flex gap-2 space-y-2 text-sm transition-colors"
+                  >
+                    <div className="bg-primary/10 text-primary flex h-10 w-10 shrink-0 items-center justify-center rounded-full text-center transition-colors">
+                      <detail.icon size={15} />
+                    </div>
+                  </a>
+                ))}
+            </div>
+          </div>
         </div>
 
         {/* Right content */}
